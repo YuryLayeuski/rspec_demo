a6d1d8ff1f97543ac0893b5a059a756c12c399bc
Yurkapur <yurkapur83@gmail.com>
2016-07-18 21:41:57 -0700

    Add something

--- hero_spec.rb
+++ hero_spec.rb
@@ -12,16 +12,19 @@ describe Hero do
   end
 
   it "can power up" do
-   expect(@hero.power_up).to eq 110
+    expect(@hero.power_up).to eq 110
   end
 
   it "can power down" do
-   expect(@hero.power_down).to eq 90
+    expect(@hero.power_down).to eq 90
   end
 
  it "display full hero info" do
-  expect(@hero.hero_info).to eq 'Mike has a health of 100'
+    expect(@hero.hero_info).to eq 'Mike has a health of 100'
  end
+
+
+ 
 
 end
 
